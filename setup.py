b784947623d8ee03bf18f181f8907ea3f2e3f1a4
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2024-11-16 22:29:11 +0100

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prompt-zen",
-    version="0.1.0",
+    version="1.0.0",
     description="A framework for iterative prompt refinement and optimization.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
